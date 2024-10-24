b8ebc7dd777a9a529f935085a0e9c6adeb8aa151
Manabodha Mahananda <manmahanand95@gmail.com>
2024-10-24 12:33:49 +0530

    first commit

---
+++ src/Page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Page.css';
+const Page = () => {
+    return (
+        <div className='main'>
+            <div className='first'>
+                <button>text_box</button>
+                <button>check_box</button>
+                <button>radio</button>
+            </div>
+            <div className='second'>
+                twoknivnimcienvinv
+            </div>
+        </div>
+    )
+}
+
+export default Page;
